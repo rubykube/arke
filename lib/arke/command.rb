a1429a34ce52c14a1c5fb164c69f145445557916
Andrey Makovenko <makovenkoandrey@gmail.com>
2019-02-13 22:45:57 +0200

    Config management (#12)
    * Update ruby version and Dockerfile

--- lib/arke/command.rb
+++ lib/arke/command.rb
@@ -1,12 +1,24 @@
 require 'clamp'
 require 'command/root'
+require 'configuration'
+require 'yaml'
 
 module Arke
   module Command
     def run!
+      load_configuration
       Root.run
     end
     module_function :run!
+
+    def load_configuration
+      configs = YAML.load(File.open('config/variables.yaml'))
+      Arke.configure do |config|
+        config.host = configs['host']
+        config.api_key = configs['api_key']
+      end
+    end
+    module_function :load_configuration
 
     # NOTE: we can add more features here (colored output, etc.)
   end
