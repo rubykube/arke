b6fcef74396be57e270a46b2b505154d787b1af9
Maryna Kalenska <mkalenska@heliostech.fr>
2019-02-15 11:38:15 +0200

    Add and refactor tests

---
+++ spec/rubykube/api_client_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'securerandom'
+
+require './lib/rubykube/api_client'
+
+RSpec.describe Rubykube::ApiClient do
+
+  let(:host)   { 'http://www.devkube.com/' }
+  let(:key)    { SecureRandom.hex }
+  let(:secret) { SecureRandom.hex }
+
+  let(:api_client) { Rubykube::ApiClient.new(host, key, secret) }
+
+  it 'sets proper header in get request' do
+    api_client_get = api_client.get('/peatio/public/timestamp')
+
+    expect(api_client_get.env.request_headers.keys).to include('X-Auth-Apikey', 'X-Auth-Nonce', 'X-Auth-Signature', 'Content-Type')
+    expect(api_client_get.env.request_headers).to include('X-Auth-Apikey' => key)
+  end
+
+  it 'gets successful answer on get request' do
+    api_client_get = api_client.get('/peatio/public/timestamp')
+
+    expect(api_client_get.env.status).to eq(200)
+    expect(api_client_get.env.reason_phrase).to eq('OK')
+  end
+
+  it 'sets proper header in post request' do
+    api_client_post = api_client.post('/barong/identity/users/email/generate_code', { email: 'admin@barong.io' })
+
+    expect(api_client_post.env.request_headers.keys).to include('X-Auth-Apikey', 'X-Auth-Nonce', 'X-Auth-Signature', 'Content-Type')
+    expect(api_client_post.env.request_headers).to include('X-Auth-Apikey' => key)
+  end
+
+end
