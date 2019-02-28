e2c5ae32a9a7647970572f3a718b61b2b9c65852
Louis Bellet <lbellet@heliostech.fr>
2019-03-01 05:52:14 +0800

    Dry run flag

--- lib/arke/command/start.rb
+++ lib/arke/command/start.rb
@@ -1,8 +1,12 @@
 module Arke
   module Command
     class Start < Clamp::Command
+
+      option "--dry", :flag, "dry run on the target"
+
       def execute
         config = Arke::Configuration.require!(:strategy)
+        pp config['dry'] = dry?
         reactor = Arke::Reactor.new(config)
         reactor.run
       end
