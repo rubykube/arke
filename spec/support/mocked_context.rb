70f98b26df733be85bcb6fc31ec2217be82dfe0e
Andrey Makovenko <makovenkoandrey@gmail.com>
2019-03-01 20:19:35 +0200

    add Binance websocket upstream (#58)

--- spec/support/mocked_context.rb
+++ spec/support/mocked_context.rb
@@ -26,6 +26,20 @@ shared_context 'mocked rubykube' do
     stub_request(:post, /peatio\/market\/orders\/\d+\/cancel/).
     with(headers: authorized_header).
     to_return(status: 201, body: '', headers: {})
+  end
+end
 
+shared_context 'mocked binance' do
+  before(:each) do
+    stub_request(:get, "https://www.binance.com/api/v1/depth?symbol=ETHUSDT&limit=1000").
+    to_return(
+      status: 200,
+      body: {
+        "lastUpdateId"=>320927259,
+        "bids"=>[["135.87000000", "36.43875000", []], ["135.85000000", "0.57176000", []], ["135.84000000", "6.62227000", []]],
+        "asks"=>[["135.91000000", "0.00070000", []], ["135.93000000", "8.00000000", []], ["135.95000000", "1.11699000", []]]
+      }.to_json,
+      headers: {}
+    )
   end
 end
