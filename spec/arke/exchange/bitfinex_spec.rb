c12a2fbaf44af092de2a59243bc05159afa59fc2
Denis <dfedorchenko@heliostech.fr>
2019-03-01 17:41:14 +0200

    Add specs for bitfinex

--- spec/arke/exchange/bitfinex_spec.rb
+++ spec/arke/exchange/bitfinex_spec.rb
@@ -1,20 +1,60 @@
 describe Arke::Exchange::Bitfinex do
-  let(:config) do
-    {
-      'driver' => 'rubykube',
-      'host' => 'http://www.devkube.com',
-      'key' => @authorized_api_key,
-      'secret' => SecureRandom.hex,
-      'market' => 'ethusd'
-    }
-  end
+  let(:config) { YAML.load_file('spec/support/fixtures/test_config.yaml') }
   let(:bitfinex_config) do
     {
-      'driver' => 'bitfinex'
+      'driver' => 'bitfinex',
+      'market' => 'tETHUSD',
+      'host' => "api.bitfinex.com",
+      'key' => "",
+      'secret' => "",
+      'rate_limit' => 1.0
     }
   end
   let(:strategy) { Arke::Strategy::Copy.new(config) }
   let(:bitfinex) { Arke::Exchange::Bitfinex.new(bitfinex_config) }
+
+  context 'Bitfinex class' do
+    let(:data_create) { [1, 10, 20] }
+    let(:data_create_sell) { [2, 22, -33] }
+    let(:data_delete) { [3, 0, 44] }
+
+    it 'initialize configuration' do
+      expect(bitfinex.orderbook).not_to be_nil
+      expect(bitfinex.instance_variable_get(:@market)).to eq(bitfinex_config['market'])
+    end
+
+    it '#build_order with positive amount' do
+      id, price, amount = data_create
+      order = bitfinex.build_order(data_create)
+
+      expect(order.id).to eq(id)
+      expect(order.price).to eq(price)
+      expect(order.amount).to eq(amount)
+      expect(order.side).to eq(:buy)
+      expect(order.market).to eq(bitfinex_config['market'])
+    end
+
+    it '#build_order with negative amount' do
+      id, price, amount = data_create_sell
+      order = bitfinex.build_order(data_create_sell)
+
+      expect(order.id).to eq(id)
+      expect(order.price).to eq(price)
+      expect(order.amount).to eq(-amount)
+      expect(order.side).to eq(:sell)
+      expect(order.market).to eq(bitfinex_config['market'])
+    end
+
+    it '#process_data creates order' do
+      expect(bitfinex.orderbook).to receive(:create)
+      bitfinex.process_data(data_create)
+    end
+
+    it '#process_data deletes order' do
+      expect(bitfinex.orderbook).to receive(:delete)
+      bitfinex.process_data(data_delete)
+    end
+  end
 
   context 'websocket messages processing' do
     let(:snapshot_message) do
@@ -36,12 +76,18 @@ describe Arke::Exchange::Bitfinex do
 
     let(:single_order) { [69586, [22814737094, 141, 2.45]] }
 
-    let(:process_snapshot_message) do
-      bitfinex.process_message(snapshot_message)
+    it 'processes single order in message' do
+      # expect(bitfinex).to receive(:process_channel_message)
+      expect(bitfinex).to receive(:process_data)
+
+      bitfinex.process_message(single_order)
     end
 
-    let(:process_single_order) do
-      bitfinex.process_message(single_order)
+    it 'processes list of orders' do
+      n = snapshot_message[1].length
+      expect(bitfinex).to receive(:process_data).exactly(n).times
+
+      bitfinex.process_message(snapshot_message)
     end
   end
 end
