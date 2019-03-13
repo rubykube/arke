a4c40a96a8694adf410d94b30e789b04e0afcb2b
denisfd <fedorchenko999@gmail.com>
2019-03-13 22:11:13 +0200

    Add amount scaling

--- lib/arke/strategy/copy.rb
+++ lib/arke/strategy/copy.rb
@@ -8,12 +8,9 @@ module Arke::Strategy
 
     # Processes orders and decides what action should be sent to @target
     def call(dax, &block)
-      ob = Arke::Orderbook.new(dax[:target].market)
       sources = dax.select { |k, _v| k != :target }
-
-      sources.each { |_key, source| ob.merge!(source.orderbook) }
-      ob.book[:buy].shift
-      ob.book[:sell].shift
+      ob = merge_orderbooks(sources, dax[:target].market)
+      ob = scale_amounts(ob)
 
       diff = dax[:target].open_orders.get_diff(ob)
 
@@ -24,18 +21,46 @@ module Arke::Strategy
 
         if !create.length.zero?
           order = create.first
-          scaled_order = Arke::Order.new(order.market, order.price, order.amount * @volume_ratio, order.side)
+          scaled_order = Arke::Order.new(order.market, order.price, order.amount, order.side)
           yield Arke::Action.new(:order_create, :target, { order: scaled_order })
         elsif !delete.length.zero?
           yield Arke::Action.new(:order_stop, :target, { id: delete.first })
         elsif !update.length.zero?
           order = update.first
           if order.amount > 0
-            scaled_order = Arke::Order.new(order.market, order.price, order.amount * @volume_ratio, order.side)
+            scaled_order = Arke::Order.new(order.market, order.price, order.amount, order.side)
             yield Arke::Action.new(:order_create, :target, { order: scaled_order })
           end
         end
       end
+    end
+
+    def scale_amounts(orderbook)
+      ob = Arke::Orderbook.new(orderbook.market)
+
+      [:buy, :sell].each do |side|
+        orderbook[side].each do |price, amount|
+          ob[side][price] = amount * @volume_ratio
+        end
+      end
+
+      ob
+    end
+
+    def merge_orderbooks(sources, market)
+      ob = Arke::Orderbook.new(market)
+
+      sources.each do |_key, source|
+        source_book = source.orderbook.clone
+
+        # discarding 1st level
+        source_book[:sell].shift
+        source_book[:buy].shift
+
+        ob.merge!(source_book)
+      end
+
+      ob
     end
   end
 end
